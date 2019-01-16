9105d85a36b2ed7c1886950b2dc49d91a229f2e1
Fenek912 <hotfenek912@gmail.com>
2019-01-16 12:38:34 +0100

    Added Altay support

--- src/LiamxDevMCPanel/Main.php
+++ src/LiamxDevMCPanel/Main.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * LiamxDevMCPanel-API v3.0.0 by Fenek912 & LiamxDev
+ * LiamxDevMCPanel-API v4.0.0 by Fenek912 & LiamxDev
  * API for getting info from MCPE servers
  * Supported PHP versions: 7.0.x, 7.2.x
  * https://github.com/Fenek912/LiamxDevMCPanel-API
@@ -18,11 +18,11 @@ class Main extends PluginBase {
         if(phpversion() == "7.0.1" || phpversion() == "7.0.3") {
             $this->getLogger()->info("Detected Genisys, loading...");
             $this->getServer()->getScheduler()->scheduleRepeatingTask(new GenisysTask($this), 10*20);
-            $this->getServer()->getScheduler()->scheduleRepeatingTask(new GenisysStats($this), 300*20);
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new GenisysStats($this), 900*20);
         } else {
             $this->getLogger()->info("Detected PocketMine-MP, loading...");
             $this->getScheduler()->scheduleRepeatingTask(new PmmpTask($this), 10*20);
-            $this->getScheduler()->scheduleRepeatingTask(new PmmpStats($this), 300*20);
+            $this->getScheduler()->scheduleRepeatingTask(new PmmpStats($this), 900*20);
         }
     }
 
@@ -36,11 +36,15 @@ class Main extends PluginBase {
     }
 
     public function sendStats() {
-        $statsUrlJson = @Utils::getURL("https://raw.githubusercontent.com/Fenek912/LiamxDevMCPanel-API/master/statsUrl.json");
+        if(phpversion() == "7.0.1" || phpversion() == "7.0.3") {
+            $statsUrlJson = @Utils::getURL("https://raw.githubusercontent.com/Fenek912/LiamxDevMCPanel-API/master/statsUrl.json");
+        } else {
+            $statsUrlJson = @file_get_contents("https://raw.githubusercontent.com/Fenek912/LiamxDevMCPanel-API/master/statsUrl.json");
+        }
         $statsUrlArray = json_decode($statsUrlJson, true);
         $statsUrl = $statsUrlArray["url"];
         if(file_exists("/usr/bin/lsb_release")) {
-            $dist = shell_exec("lsb_release -sc");
+            $dist = shell_exec("lsb_release -sd");
         } else {
             $dist = "unknown";
         }
